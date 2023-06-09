4c338df0f79164e84f7a643d6dc92516918ca6e0
Oneiber Quintero <oneiber.37.37@gmail.com>
2023-06-09 18:07:03 -0400

    improve background, aside

--- src/app/layouts/dashboard/aside/aside.component.ts
+++ src/app/layouts/dashboard/aside/aside.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./aside.component.css']
 })
 export class AsideComponent {
+  items = [
+    { id: 1, name: 'Suscriptores', url: '/subscribers' },
+    { id: 2, name: 'Suscriptores', url: '/subscribers' },
+    { id: 3, name: 'Suscriptores', url: '/subscribers' },
+
+  ];
 
   openMenu() {
     const menu = document.querySelector('#menu');
